e5decdd0d0b8811ee237398f5c7e8d4b0952602a
kellirestivo <kellirestivo@gmail.com>
2024-06-25 23:04:22 -0500

    add imports to init for convenience

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,4 @@ from .data_extraction import process_mat_file
 from .data_saving import save_as_pickle
 from .database_config import setup_configuration
 
-__all__ = ['get_filename', 'process_mat_file', 'save_as_pickle', 'setup_configuration']
+__all__ = ['get_filename', 'process_mat_file', 'save_as_pickle', 'setup_configuration'] 
