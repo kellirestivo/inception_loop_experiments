2eed010fcace6ad7ce7a9d3afe581d71939a8694
kellirestivo <kellirestivo@gmail.com>
2024-06-26 00:22:18 -0500

    add MEI shifting imports to init

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,9 @@
 from .input_handling import get_filename
 from .data_extraction import process_mat_file
 from .data_saving import save_as_pickle
-from .database_config import setup_configuration
+from .config_setup import setup_configuration
+from .mei_shifting import shift_based_on_rf, center_of_mass, shift
 
-__all__ = ['get_filename', 'process_mat_file', 'save_as_pickle', 'setup_configuration'] 
+__all__ = ['get_filename', 'process_mat_file', 'save_as_pickle', 
+           'setup_configuration', 'shift_based_on_rf', 'center_of_mass',
+           'shift'] 
