ea120db46feb996b6a4e5dda8676d35ceac77738
kellirestivo <kellirestivo@gmail.com>
2024-06-26 15:05:34 -0500

    add dot map position function and make docstrings more similar

--- src/input_handling.py
+++ src/input_handling.py
@@ -1,9 +1,33 @@
 def get_filename():
     """
-    Prompt user to input root filename of current experiment.
+    Prompts user for root filename of current experiment.
 
     Returns:
     str: The root filename input by the user.
     """
 
     return input("Input root filename = ")
+
+def get_dotmap_position():
+    """
+    Prompts the user for x and y coordinates and returns them as a tuple.
+
+    Returns:
+    tuple: (user_input_x, user_input_y) coordinates in monitor space (1920x1080).
+    """
+    while True:
+        try:
+            user_input_x = int(input("Please enter the x coordinate (in monitor space 1920x1080): "))
+            user_input_y = int(input("Please enter the y coordinate (in monitor space 1920x1080): "))
+            
+            if 0 <= user_input_x <= 1920 and 0 <= user_input_y <= 1080:
+                break
+            else:
+                print("Coordinates must be within the range (0, 0) to (1920, 1080). Please try again.")
+        except ValueError:
+            print("Invalid input. Please enter integer values for the coordinates.")
+
+    dotmap_position = (user_input_x, user_input_y)
+    print(f"Dotmap position: {dotmap_position}")
+
+    return dotmap_position
