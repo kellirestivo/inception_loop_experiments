c45b02150b2772dd06f421ba32bb7384bb1ff0b1
kellirestivo <kellirestivo@gmail.com>
2024-06-25 21:41:14 -0500

    add modularity to input current dataset filename for processing

---
+++ input_handling.py
@@ -0,0 +1,9 @@
+def get_filename():
+    """
+    Prompt user to input root filename of current experiment.
+
+    Returns:
+    str: The root filename input by the user.
+    """
+
+    return input("Input root filename = ")
